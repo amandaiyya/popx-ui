bf47f04acbf99645dd56958d43b15fd9c523aebf
amansain11 <sainaman1819@gmail.com>
2025-09-17 21:53:47 +0530

    Added routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import Layout from './Layout.jsx';
+
+import { 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  Route, 
+  RouterProvider 
+} from 'react-router-dom';
+
+import {
+  HomePage,
+  SignupPage,
+  LoginPage,
+  ProfilePage,
+} from './pages';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    // root route
+    <Route path='/' element={<Layout />}>
+      {/* children routes */}
+      <Route index element={<HomePage />}/>
+      <Route path='signup' element={<SignupPage />}/>
+      <Route path='login' element={<LoginPage />}/>
+      <Route path='profile' element={<ProfilePage />}/>
+
+      {/* Route for The Wildcard Paths */}
+      <Route path='*' element={
+        <>
+          <p>404 Page Not found</p>
+        </>
+      }/>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
